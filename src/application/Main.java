6d5c13456a1a3e02aff607ad81dc15115f52f566
yeyulchoi <yeyulchoi@outlook.com>
2023-01-09 17:23:49 -0500

    created classes for OOP for connection to DB

--- src/application/Main.java
+++ src/application/Main.java
@@ -19,77 +19,97 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
+
+	
+	}
+	
+	public static void main(String[] args) throws SQLException {
 		
-		try {
-			Connection conn = null;
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3307/satishlesson","root","Yoonja7979!!");
-			System.out.println("database connected successfully");
-			  //ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-			
-			Scanner scan = new Scanner(System.in);
-			
-			System.out.println("Please enter your name.");
-			String userName= scan.next();
-			System.out.println("Please enter your  password.");
-			String userpw=scan.next();
-			System.out.println("Please enter your country ");
-			String usercountry = scan.next();
-			System.out.println("Please enter your mark ");
-			Double userMark = scan.nextDouble();
-			
-			String query = "insert into student values(?,?,?,?);";
-		
-//<<<<<<<<<<<<<<<<<<USE of PrepareStatement to mapping input from user to ? >>>>>>>>>>>>>>>>>>>			
-			PreparedStatement stmt = conn.prepareStatement(query);
-			stmt.setString(1, userName);
-			stmt.setString(2,userpw);
-			stmt.setString(3, usercountry);
-			stmt.setDouble(4, userMark);
-			
-			stmt.execute();
-			System.out.println("User input has been inserted to database");
-			stmt.close();
-			conn.close();
-				
-			
-//			ResultSet result = stmt.executeQuery("select * from student");
-//			
-//			if(!result.next()) {
-//				System.out.println("NO SEARCH FROM THE TABLE.");
-//			}else 
-//			{
-//				
-//				result.previous();
-//				while(result.next()) {
-//				System.out.println(result.getString(1)+"    "+result.getString(2)+"  "+result.getString(3)+"   "+result.getInt(4));
-//			}
-			
-			
-			
-			
-			
-		
-			conn.close();
-			
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Student stu = new Student();
+		stu.insertStudent();
 		
 		
 		
 		
 		
 		
+	}
+}
+
+class Student
+{
+	private String userName;
+	private String userpw;
+	private String usercountry ;
+	private double userMark;
+//getter and setter
+	
+	public void setUserInputInfo() {
+		Scanner scan = new Scanner(System.in);
 		
+		System.out.println("Please enter your name.");
+		userName= scan.next();
+		System.out.println("Please enter your  password.");
+		userpw=scan.next();
+		System.out.println("Please enter your country ");
+		usercountry = scan.next();
+		System.out.println("Please enter your mark ");
+		 userMark = scan.nextDouble();
 		
-		
-		
-		
+		 System.out.println("user completed input into database.");
+		 
+		 scan.close();
 		
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
+	public void insertStudent() throws SQLException {
+		
+		//here  we are going to work with a database
+		//we need to open a database connection
+		
+		DBMSconnection mydbconn= new DBMSconnection("jdbc:mysql://localhost:3307/satishlesson","root","Yoonja7979!!");
+		Connection con =mydbconn.getConnection();
+		Student stdnt = new Student();
+		stdnt.setUserInputInfo();
+		String query = "insert into student values(?,?,?,?);";
+		PreparedStatement stmt = con.prepareStatement(query);
+		stmt.setString(1, stdnt.userName);
+		stmt.setString(2,stdnt.userpw);
+		stmt.setString(3, stdnt.usercountry);
+		stmt.setDouble(4, stdnt.userMark);
+		
+		stmt.execute();
+		System.out.println("User input has been inserted to database");
+		mydbconn.closeConnection(con, stmt);
+	    
 	}
+	
+}
+
+class DBMSconnection
+{
+	String  url;
+	String  username;
+	String  password;
+	
+	// the three above need to be initialized.
+	public DBMSconnection(String url, String username, String password) {
+		this.url = url;
+		this.username = username;
+		this.password = password;
+	}
+	
+	public Connection getConnection() throws SQLException {
+		Connection conn = null;
+		conn = DriverManager.getConnection(url,username,password);
+		System.out.println("database connected successfully");
+		return conn;
+	}
+
+	public void closeConnection(Connection con, Statement stmt) throws SQLException {
+		con.close();
+		stmt.close();
+		System.out.println("The connection is closed");
+	}
+	
 }
